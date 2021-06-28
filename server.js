0db79369decffa5962b9e56f79e25f164e581d6b
Mr Dev <belkoweb9718@gmail.com>
2021-06-28 23:07:31 +0100

    Code Refractoring

--- server.js
+++ server.js
@@ -2,20 +2,24 @@ const express = require("express");
 const socketIO = require("socket.io");
 
 const PORT = process.env.PORT || 3000;
-
+//creation d'une serveur express ecoutant sur le port 3000 
 const server = express().listen(PORT, () =>
   console.log(`Listening on ${PORT}`)
 );
+//creation d'une instance socketIO
 const io = socketIO(server);
 io.on("connection", (socket) => {
+  //deconnexion du client 
   socket.on("disconnect", () => console.log("Client disconnected"));
+  //emit des infos de la vitesse
   socket.on("speed", (message) => {
     io.emit("test", message.response);
   });
-  // comment
+  // emit des images de la camera 
   socket.on("data", (data) => {
     io.emit("image", data);
   });
+  //emit des infos directionnelles
   socket.on("direction", (data) => {
     io.emit("turn", data);
   });
